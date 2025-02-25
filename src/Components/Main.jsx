a465f9fabd60626c2c628b350c5258a138dbfd91
Joseph Arteaga <jarteaga127@gmail.com>
2025-02-25 10:50:04 +0900

    small changes to css file

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -26,8 +26,13 @@ const Main = () => {
             <section className="about-us">
                 <article>
                 <h1>Little Lemon</h1>
-                <h2>Tokyo</h2>    
+                <h2>Tokyo</h2>
+                <p>Kyoto ya Nara ni wa, nihon no furui bunka ga nokoteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitte ireba, Kyoto ya Nara wo otozureru kachi ga motto fueru to omou.</p>    
+                <button className="main-btn">Read more about our story here</button>
                 </article>
+                <div className="two-imgs">
+                    
+                </div>
             </section>
         </main>
         </>
---
+++ src/Components/ReviewCard.jsx
@@ -0,0 +1,14 @@
+const ReviewCard = () => {
+    return ( 
+        <div className="review-card">
+          <div className="review-rating"></div>
+          <div className="review-user">
+            <div className="user-avatar"></div>
+            <div className="review-username">Tommy Gunn</div>
+          </div>
+            <div className="review-descript">Kyoto ya Nara ni wa, nihon no furui bunka ga nokoteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. </div>
+        </div>
+     );
+}
+ 
+export default ReviewCard;
