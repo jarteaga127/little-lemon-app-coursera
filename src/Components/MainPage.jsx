5908f31dea4135f89b55967db6d1e4c5b64cd294
Joseph Arteaga <jarteaga127@gmail.com>
2025-03-07 13:23:53 +0900

    added some minor changes to the footer

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -42,9 +42,11 @@ const MainPage = () => {
                 <p>Kyoto ya Nara ni wa, nihon no furui bunka ga nokoteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitte ireba, Kyoto ya Nara wo otozureru kachi ga motto fueru to omou.</p>    
                 <button className="main-btn">Read more about our story here</button>
                 </article>
+                <div className="img-contain-about">
                 <div className="two-imgs">
                  <img src={RestaurantImg} alt="" className="image-1" />
                  <img src={TwoChefs} alt="" className="image-2" />   
+                </div>
                 </div>
             </section>
         </main>
